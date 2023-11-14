1fd825729231789e773ef9cf9764c1cb26139346
wo2ni <i_o_i_o@163.com>
2023-11-14 21:24:41 +0800

    config bash-completion

---
+++ config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#--TAB自动补全.
+cat << "EOF" >>/etc/bash.bashrc
+# enable bash completion in interactive shells
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+EOF
