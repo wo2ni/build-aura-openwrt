f591c66efc518a45aa69d691092bd84110b3ed53
wo2ni <i_o_i_o@163.com>
2024-04-02 17:28:54 +0800

    :rocket: po2lmo package

---
+++ po2lmo/mkversion.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+cat <<EOF > $1
+local pcall, dofile, _G = pcall, dofile, _G
+
+module "luci.version"
+
+if pcall(dofile, "/etc/openwrt_release") and _G.DISTRIB_DESCRIPTION then
+	distname    = ""
+	distversion = _G.DISTRIB_DESCRIPTION
+	if _G.DISTRIB_REVISION then
+		distrevision = _G.DISTRIB_REVISION
+		if not distversion:find(distrevision,1,true) then
+			distversion = distversion .. " " .. distrevision
+		end
+	end
+else
+	distname    = "OpenWrt"
+	distversion = "Development Snapshot"
+end
+
+luciversion = "💗出生日期: $(TZ='Asia/Shanghai' date "+%Y年%m月%d日 %H时%M分%S秒")💗"
+EOF
+#luciname    = "${3:-LuCI}"
+#luciversion = "${2:-Git}"
