1a4e798ec442c6745ed43d5d77948f685170bfd6
wo2ni <i_o_i_o@163.com>
2025-07-18 10:59:34 +0800

    rm -rf *

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-docker build ./ -f Dockerfile-ubuntu20.04 -t make_openwrt:v1.0
-#docker build ./ -f Dockerfile -t make_openwrt:v1.0
-#docker save make_openwrt:v1.0 >make_openwrt_v1.0.tar
